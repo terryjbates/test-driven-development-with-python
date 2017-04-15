c7b7f3709fc958a2c694858c65a379cfcd54f72b
Terry Bates <terryjbates@gmail.com>
2017-04-15 00:08:45 -0700

    Use less verbose byte string syntax

--- myflaskapp/tests/test_unit.py
+++ myflaskapp/tests/test_unit.py
@@ -9,13 +9,11 @@ class SmokeTest(unittest.TestCase):
     def test_home_page_is_about_todo_lists(self):
         request = requests.get('http://localhost:5000')
         self.assertTrue(
-            request.content.startswith(bytes('\n\n<!doctype html>\n', 'utf-8')))
+            request.content.startswith(b'\n\n<!doctype html>\n'))
         self.assertIn(
             '<title>\n  \n    tdd_with_python\n  \n    \n  </title>\n',
             request.text)
-        self.assertTrue(
-            request.content.endswith(bytes('</body>\n</html>\n', 'utf-8')))
-
+        self.assertTrue(request.content.endswith(b'</body>\n</html>\n'))
 
 class TestMainPage:
     """WebTest test for title"""
@@ -29,3 +27,6 @@ class TestMainPage:
     def test_main_page_returns_expected_title(self, user, testapp):
         res = testapp.get('/')
         assert '<title>\n  \n    tdd_with_python\n  \n    \n  </title>\n' in res
+
+    # def test_main_page_returns_expected_content(self, user, testapp):
+    #     res = testapp.get('/')
