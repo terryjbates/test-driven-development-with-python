28c7b45f52e3824298598f66310ea2338c15c506
Terry Bates <terryjbates@gmail.com>
2017-04-15 03:24:19 -0700

    Add test_client.py, add SERVER_NAME to config

---
+++ myflaskapp/tests/test_client.py
@@ -0,0 +1,24 @@
+import unittest
+from myflaskapp.app import create_app
+from myflaskapp.database import db as _db
+from myflaskapp.settings import TestConfig
+
+from .factories import UserFactory
+from .conftest import app
+from flask import url_for
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self):
+        """An application for the tests."""
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_home_page(self):
+        pass
+        response = self.client.get(url_for('public.home'))
+    #     self.assertTrue('Stranger' in response.get_data(as_text=True))
