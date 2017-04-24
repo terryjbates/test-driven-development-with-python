52c17d6052563b80c713b8cb431171bdd1118163
Terry Bates <terryjbates@gmail.com>
2017-04-23 21:33:28 -0700

    Create the 'item' subdir with a models module

---
+++ myflaskapp/myflaskapp/item/models.py
@@ -0,0 +1,11 @@
+"""User models."""
+import datetime as dt
+
+from flask_login import UserMixin
+
+from myflaskapp.database import Column, Model, SurrogatePK, db, reference_col, relationship
+from myflaskapp.extensions import bcrypt
+
+class Item(SurrogatePK, Model):
+    __tablename__ = 'items'
+    pass
