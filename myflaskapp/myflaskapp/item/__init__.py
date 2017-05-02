b2f521336bdc62d7c079afe751d7a49edaacb004
Terry Bates <terryjbates@gmail.com>
2017-05-02 16:48:52 -0700

    Make item dir import as package, add SQLAlchemy relationship to Item model

---
+++ myflaskapp/myflaskapp/item/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""The item module."""
+from . import models  # noqa
--- myflaskapp/myflaskapp/item/models.py
+++ myflaskapp/myflaskapp/item/models.py
@@ -8,5 +8,15 @@ from myflaskapp.extensions import bcrypt
 
 class Item(SurrogatePK, Model):
     __tablename__ = 'items'
+    __table_args__ = {'extend_existing': True}
     text = Column(db.String(80),nullable=True)
+    user_id = reference_col('users', nullable=True)
+    user = relationship('User', backref='items')
+    #user = Column(db.String(64), db.ForeignKey('users.username'))
+    
+    def __init__(self, text=None, user=None):
+        self.user = user
+        self.text = text
+
+
 
